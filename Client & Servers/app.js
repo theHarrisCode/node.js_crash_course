170ac6a8999e1893e738b1bcbfa9cb2b3346bc9a
Darren Harris <theharriscode401@gmail.com>
2024-01-12 15:30:15 -0500

    adding blogs to homepage using ejs

--- Client & Servers/app.js
+++ Client & Servers/app.js
@@ -15,18 +15,23 @@ app.listen(3000)
 
 // sending index.html to browswer
 app.get('/', (req,res) => {
+    const blogs = [
+        {title: "Jayson Tatum scores 45 points", snippet: "random text random text random text random text "},
+        {title: "Jaylen brown dishes out 8 assists", snippet: "random text random text random text random text "},
+        {title: "Kristaps Porzingis grabs 14 rebounds", snippet: "random text random text random text random text "}
+    ]
     // res.sendFile('./views/index.html', {root: __dirname}); sending a file to the browser
-    res.render('index'); // render() will automatically look inside of 'views' folder
+    res.render('index', {title: 'Home', blogs}); // render() will automatically look inside of 'views' folder
 });
 
 // sending about.html to browser
 app.get('/about', (req, res) => {
     // res.sendFile('./views/about.html', { root: __dirname});
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create a new Blog'});
 })
 // redirecting about-us to about page
 // app.get('/about-us', (req, res) => {
@@ -35,5 +40,5 @@ app.get('/blogs/create', (req, res) => {
 
 // default response if none of the requested pages are valid | 404 error & setting the status code
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'});
 });
